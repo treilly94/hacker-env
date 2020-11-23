89e43e156693fc79b94a57995b307fdda7e7a831
Tom Reilly <tom@thinkpad.home>
2020-11-23 22:59:18 +0000

    added descriptions to setup

--- password-cracking/.terragrunt-cache/5iR2SzTO_7uR4bBZxXlNh2G3s68/y5u8zevxWKfGmd8iQRJQR0ehdKs/setup.sh
+++ password-cracking/.terragrunt-cache/5iR2SzTO_7uR4bBZxXlNh2G3s68/y5u8zevxWKfGmd8iQRJQR0ehdKs/setup.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+update() {
+    echo "Updating apt"
+    apt-get update -y
+}
+
 create_user() {
+    echo "Adding $1"
     useradd -m -s /bin/bash $1
     echo "$1:$2" | chpasswd
 }
 
 configure_ssh() {
+    echo "Configuring ssh"
     sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
     systemctl restart sshd.service
 }
 
-apt-get update -y
+update
 
 create_user jeremy ymerej
 create_user dave budlight
